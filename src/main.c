4c46485e41b48c0e761d44d345cdfc3d1007f1fb
Arnel A. Borja <kyoushuu@yahoo.com>
2013-10-10 14:59:51 +0800

    main: Implemented connecting to server

--- src/main.c
+++ src/main.c
@@ -18,8 +18,116 @@
  */
 
 #include <stdio.h>
-int main()
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+
+#define HOST "irc.rizon.net"
+#define PORT "6667"
+#define CHANNEL "#otakubytes"
+#define NICK "kona-bot"
+#define NAME "Izumi Konata"
+
+#define MAXDATASIZE 512
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr (struct sockaddr *sa)
 {
-    printf("Hello world\n");
-    return (0);
+    if (sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main (int argc, char *argv[])
+{
+    int sockfd, numbytes, msglen, ret;
+    char buf[MAXDATASIZE], msg[MAXDATASIZE], *newline;
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
+    memset (&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo (HOST, PORT, &hints, &servinfo)) != 0)
+    {
+        fprintf (stderr, "getaddrinfo: %s\n", gai_strerror (rv));
+        return 1;
+    }
+
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
+        if ((sockfd = socket (p->ai_family, p->ai_socktype,
+                              p->ai_protocol)) == -1)
+        {
+            perror ("socket");
+            continue;
+        }
+
+        if (connect (sockfd, p->ai_addr, p->ai_addrlen) == -1)
+        {
+            close (sockfd);
+            perror ("connect");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL)
+    {
+        fprintf (stderr, "Failed to connect\n");
+        return 2;
+    }
+
+    inet_ntop (p->ai_family, get_in_addr ((struct sockaddr *)p->ai_addr),
+               s, sizeof s);
+    printf ("Connecting to %s\n", s);
+
+    freeaddrinfo (servinfo);
+
+    numbytes = 0;
+    buf[0] = '\0';
+    while (1)
+    {
+        char *msgtype, *recipient, *sender, *contents;
+
+        while (!(newline = strstr (buf, "\r\n")))
+        {
+            ret = recv (sockfd, buf + numbytes, MAXDATASIZE - numbytes - 1, 0);
+            if (ret == -1)
+            {
+                perror ("recv");
+                break;
+            }
+            else if (ret == 0)
+                break;
+
+            numbytes += ret;
+        }
+
+        msglen = newline + 2 - buf;
+        strncpy (msg, buf, numbytes);
+        numbytes -= msglen;
+        strncpy (buf, msg + msglen, numbytes);
+        msg[msglen - 2] = '\0';
+        buf[numbytes] = '\0';
+
+        printf ("%s\n", msg);
+    }
+
+    close (sockfd);
+
+    return 0;
 }
