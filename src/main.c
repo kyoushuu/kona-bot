cd3a41a8b1a97f482cfd1f4e47d45b1f2a499c37
Arnel A. Borja <kyoushuu@yahoo.com>
2013-10-11 00:45:42 +0800

    main: Use irc module

--- src/main.c
+++ src/main.c
@@ -19,103 +19,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <netinet/in.h>
+#include <unistd.h>
 #include <sys/socket.h>
-
-#include <arpa/inet.h>
 
 #include <mysql.h>
 
 #include "database.h"
 #include "io-utils.h"
+#include "irc.h"
 
-#define HOST "irc.rizon.net"
-#define PORT "6667"
-#define CHANNEL "#otakubytes"
-#define NICK "kona-bot"
-#define NAME "Izumi Konata"
-
-#define MAXDATASIZE 512
-
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr (struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET)
-    {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
 
 int main (int argc, char *argv[])
 {
     int sockfd, numbytes, msglen, ret;
     char buf[MAXDATASIZE], msg[MAXDATASIZE], *newline;
     char res[MAXDATASIZE];
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    char s[INET6_ADDRSTRLEN];
+    char *irc_host, *irc_port, *irc_chan, *irc_nick, *irc_name;
 
     MYSQL *db;
 
-    memset (&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-
-    if ((rv = getaddrinfo (HOST, PORT, &hints, &servinfo)) != 0)
+    sockfd = open_irc_socket (&irc_host, &irc_port, &irc_chan, &irc_nick, &irc_name);
+    if (sockfd < 0)
     {
-        fprintf (stderr, "getaddrinfo: %s\n", gai_strerror (rv));
         return 1;
     }
 
-    for (p = servinfo; p != NULL; p = p->ai_next)
-    {
-        if ((sockfd = socket (p->ai_family, p->ai_socktype,
-                              p->ai_protocol)) == -1)
-        {
-            perror ("socket");
-            continue;
-        }
-
-        if (connect (sockfd, p->ai_addr, p->ai_addrlen) == -1)
-        {
-            close (sockfd);
-            perror ("connect");
-            continue;
-        }
-
-        break;
-    }
-
-    if (p == NULL)
-    {
-        fprintf (stderr, "Failed to connect\n");
-        return 2;
-    }
-
-    inet_ntop (p->ai_family, get_in_addr ((struct sockaddr *)p->ai_addr),
-               s, sizeof s);
-    printf ("Connecting to %s\n", s);
-
-    freeaddrinfo (servinfo);
-
-    snprintf (res, MAXDATASIZE, "NICK %s\r\n", NICK);
-    send_all (sockfd, res);
-
-    snprintf (res, MAXDATASIZE, "USER %s 0 * :%s\r\n", NICK, NAME);
-    send_all (sockfd, res);
-
-    snprintf (res, MAXDATASIZE, "JOIN %s\r\n", CHANNEL);
-    send_all (sockfd, res);
-
     db = open_database ();
-
     if (db == NULL)
     {
         fprintf (stderr, "%s\n", mysql_error (db));
@@ -182,6 +112,12 @@ int main (int argc, char *argv[])
 
     mysql_close (db);
     close (sockfd);
+
+    free (irc_host);
+    free (irc_port);
+    free (irc_chan);
+    free (irc_nick);
+    free (irc_name);
 
     return 0;
 }
